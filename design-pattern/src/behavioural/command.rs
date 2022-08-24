df3b6fb623bb39d807346ff450e4122acbd6b64e
Aoi <ht.so.ik0102@gmail.com>
2022-08-24 17:50:37 +0700

    Update design pattern 2

--- design-pattern/src/behavioural/command.rs
+++ design-pattern/src/behavioural/command.rs
@@ -1,32 +1,38 @@
+type FnPtr = fn() -> String;
 pub trait Migration {
     fn execute(&self) -> &str;
     fn rollback(&self) -> &str;
 }
 
-pub struct CreateTable;
-impl Migration for CreateTable {
-    fn execute(&self) -> &str {
-        "create table"
-    }
-
-    fn rollback(&self) -> &str {
-        "drop table"
-    }
+pub struct Command {
+    execute: FnPtr,
+    rollback: FnPtr,
 }
 
-pub struct AddField;
-impl Migration for AddField {
-    fn execute(&self) -> &str {
-        "add field"
-    }
+// pub struct CreateTable;
+// impl Migration for CreateTable {
+//     fn execute(&self) -> &str {
+//         "create table"
+//     }
 
-    fn rollback(&self) -> &str {
-        "remove field"
-    }
-}
+//     fn rollback(&self) -> &str {
+//         "drop table"
+//     }
+// }
+
+// pub struct AddField;
+// impl Migration for AddField {
+//     fn execute(&self) -> &str {
+//         "add field"
+//     }
+
+//     fn rollback(&self) -> &str {
+//         "remove field"
+//     }
+// }
 
 struct Schema {
-    commands: Vec<Box<dyn Migration>>,
+    commands: Vec<Command>,
 }
 
 impl Schema {
@@ -34,21 +40,29 @@ impl Schema {
         Self { commands: vec![] }
     }
 
-    fn add_migration(&mut self, cmd: Box<dyn Migration>) {
-        self.commands.push(cmd)
+    fn add_migration(&mut self, execute: FnPtr, rollback: FnPtr) {
+        self.commands.push(Command { execute, rollback })
     }
 
-    fn execute(&self) -> Vec<&str> {
-        self.commands.iter().map(|cmd| cmd.execute()).collect()
+    fn execute(&self) -> Vec<String> {
+        self.commands.iter().map(|cmd| (cmd.execute)()).collect()
     }
 
-    fn rollback(&self) -> Vec<&str> {
+    fn rollback(&self) -> Vec<String> {
         self.commands
             .iter()
             .rev()
-            .map(|cmd| cmd.rollback())
+            .map(|cmd| (cmd.rollback)())
             .collect()
     }
+}
+
+fn add_field() -> String {
+    "add field".to_string()
+}
+
+fn remove_field() -> String {
+    "remove field".to_string()
 }
 
 #[cfg(test)]
@@ -59,10 +73,10 @@ mod test {
     fn test_migration() {
         let mut schema = Schema::new();
 
-        let cmd = Box::new(CreateTable);
-        schema.add_migration(cmd);
-        let cmd = Box::new(AddField);
-        schema.add_migration(cmd);
+        // let cmd = Box::new(CreateTable);
+        schema.add_migration(|| "create table".to_string(), || "drop table".to_string());
+        // let cmd = Box::new(AddField);
+        schema.add_migration(add_field, remove_field);
 
         assert_eq!(vec!["create table", "add field"], schema.execute());
         assert_eq!(vec!["remove field", "drop table"], schema.rollback());
