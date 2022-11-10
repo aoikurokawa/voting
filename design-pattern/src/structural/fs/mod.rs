c2b545a804f9b4a31176f938167cc9fbccc46353
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-10 12:04:08 +0700

    Update design pattern for composite

---
+++ design-pattern/src/structural/fs/mod.rs
@@ -0,0 +1,19 @@
+mod file;
+mod folder;
+
+pub use file::File;
+pub use folder::Folder;
+
+pub trait Component {
+    fn search(&self, keyword: &str);
+}
+
+#[cfg(test)]
+mod test {
+    use super::file::File;
+
+  fn test_composite() {
+        let file1 = File::new("File 1");
+        let file2 = File::new("File 2");
+  }  
+}
--- design-pattern/src/structural/mod.rs
+++ design-pattern/src/structural/mod.rs
@@ -1,5 +1,7 @@
 mod compose_structs;
+mod fs;
 mod prefer_small_crates;
 
 pub use compose_structs::*;
 pub use prefer_small_crates::*;
+mod fs;
